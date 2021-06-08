3dfef8163cdc7bb40b7785f9d54d6e9164dde794
Douglas Mariz <douglasmariz.developer@gmail.com>
2021-06-08 10:56:33 -0300

    primeiro commit

---
+++ src/main/java/com/java/indra/javaindra/JavaIndraApplication.java
@@ -0,0 +1,13 @@
+package com.java.indra.javaindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIndraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIndraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/indra/javaindra/JavaIndraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.indra.javaindra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIndraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
